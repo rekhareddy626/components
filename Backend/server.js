42f0b4a183ca25919e3979bb96d62e6e86801072
rekha <rekhareddy544@gmail.com>
2026-02-03 14:30:13 +0530

    Express Created

---
+++ Backend/server.js
@@ -0,0 +1,36 @@
+const express=require('express');
+const todoRoute=require("./router/todoRouter")
+const dotenv=require("dotenv");
+const userRoute=require("./router/userRouter");
+dotenv.config();
+const app=express();
+app.use(express.json())
+app.use("/api/todo",todoRoute);
+app.use("/api/user",userRoute);
+
+
+const PORT=process.env.PORT || 3000
+app.listen(PORT,()=>{
+    console.log(`Server is running on port http://localhost:${PORT}`);
+})
+
+
+// app.get("/",(req,res)=>{
+//     res.send("Get Route is Working")
+// })
+// app.post("/create",(req,res)=>{
+//     res.json({
+//         message:"Post Route is Working"
+//     })
+
+// })
+// app.put("/",(req,res)=>{
+//     res.status(200).json({
+//         message:"Put Route is Working"
+//     })
+// })
+// app.delete("/",(req,res)=>{
+//     res.status(200).json({
+//         message:"Delete Route is Working"
+//     })
+// })
